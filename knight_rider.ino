b30437bd4114268e7606d90874ce683d008c8441
KrzArpon <30576159+KrzArpon@users.noreply.github.com>
2019-04-02 19:10:48 +0600

    Add files via upload

---
+++ knight_rider.ino
@@ -0,0 +1,38 @@
+void setup() {
+  for(int pin=2; pin<14; pin++){
+    pinMode(pin, OUTPUT);
+    
+  }
+}
+
+void loop (){
+
+
+  int t = 20;
+//---------->>>for this side
+  for(int i=2; i<14; i++) {
+    digitalWrite(i, HIGH);
+    delay(t);
+    digitalWrite(i+1, HIGH);
+    delay(t);
+    digitalWrite(i+2, HIGH);
+    delay(t);
+    digitalWrite(i, LOW);
+    delay(t);
+    digitalWrite(i+1, LOW);
+    
+    
+  }
+//<<<--------------- For this side
+  for(int i=13; i>1; i--){
+    digitalWrite(i, HIGH);
+    delay(t);
+    digitalWrite(i-1, HIGH);
+    delay(t);
+    digitalWrite(i-2, HIGH);
+    delay(t);
+    digitalWrite(i, LOW);
+    delay(t);
+    digitalWrite(i-1, LOW);
+  }
+}
